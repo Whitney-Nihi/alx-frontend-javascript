fbfcf061949c080993a064175fad692d545d4c64
Whitney-Nihi <whitney.taire1@gmail.com>
2024-10-09 21:40:01 +0000

    Created advanced types

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,56 @@
+// DirectorInterface with 3 methods
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// TeacherInterface with 3 methods
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Class Director implementing DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// Class Teacher implementing TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Function to create an Employee (either a Teacher or Director)
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+console.log(createEmployee(200));  // Output: Teacher instance
+console.log(createEmployee(1000));  // Output: Director instance
+console.log(createEmployee('$500'));  // Output: Director instance
