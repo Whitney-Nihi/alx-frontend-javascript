c6697f57c8c36cd8e93a9291afac8b3effe973b0
Whitney-Nihi <whitney.taire1@gmail.com>
2024-10-09 21:18:28 +0000

    Added printteacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,14 +9,11 @@ interface Teacher {
 
 interface Directors extends Teacher {
   numberOfReports: number;      // Required for Directors
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
 };
-
-console.log(director1);
